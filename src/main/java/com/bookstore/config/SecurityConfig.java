c2d799ab1a3916f3835878a64266369faf181503
Zaya <sainzayaz65@gmail.com>
2026-09-10 14:29:30 +0800

    cart

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.bookstore.config;
 
-import java.text.Normalizer.Form;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
